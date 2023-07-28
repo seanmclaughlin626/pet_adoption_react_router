9f50bf1175603c8b1de8d89eb40deb4b906ea29a
Sean McLaughlin <sean.charles.mclaughlin@gmail.com>
2023-07-28 13:06:33 -0400

    Tentatively finished project

---
+++ starting_code/src/App.js
@@ -0,0 +1,24 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Root/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path=":type" element={<HomePage/>}/>
+    <Route path=":type/:id" element={<PetDetailsPage/>}/>
+    <Route path="search" element={<SearchPage/>}/>
+    <Route path="pet-details-not-found" element={<PetDetailsNotFound/>}/>
+  </Route>
+));
+
+function App() {
+  return (
+    <RouterProvider router = {appRouter} />
+  );
+}
+
+export default App;
